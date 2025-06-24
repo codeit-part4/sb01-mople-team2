3cfa4a5a4e4ae35233022a04c0ff2e4ed02b7c83
janghoosa <janghoosa@gmail.com>
2025-06-24 15:36:11 +0900

    ci: debug url

--- src/test/java/com/sprint/mople/MopleApplicationTests.java
+++ src/test/java/com/sprint/mople/MopleApplicationTests.java
@@ -1,6 +1,8 @@
 package com.sprint.mople;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -8,8 +10,13 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 class MopleApplicationTests {
 
+  @Value("${spring.datasource.url}")
+  String dbUrl;
+
   @Test
   void contextLoads() {
+    System.out.println("🔍 DB URL: " + dbUrl);
+    Assertions.assertTrue(dbUrl.contains("postgres"));
   }
 
 }
