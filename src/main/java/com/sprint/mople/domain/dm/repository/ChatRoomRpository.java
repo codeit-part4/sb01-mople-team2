078679b90d404aaacaf93732829ca85340ca77db
eunseobb <seobseob0810@naver.com>
2025-06-24 18:17:35 +0900

    fix: 중복 FollowRepository 수정

--- src/main/java/com/sprint/mople/domain/dm/repository/ChatRoomRpository.java
+++ src/main/java/com/sprint/mople/domain/dm/repository/ChatRoomRpository.java
@@ -1,9 +1,10 @@
 package com.sprint.mople.domain.dm.repository;
 
+import com.sprint.mople.domain.dm.entity.ChatRoom;
 import com.sprint.mople.domain.follow.entity.Follow;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FollowRepository extends JpaRepository<Follow, UUID> {
+public interface ChatRoomRpository extends JpaRepository<ChatRoom, UUID> {
 
 }
