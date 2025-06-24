aee1690c3a28cfd663817ccb83c71021cc63b5ee
Haram0111 <5gkfka5@naver.com>
2025-06-24 16:12:47 +0900

    feat: 비밀번호 암호화 - SecurityConfig에 passwordEncoder 추가 - BCrypt 방식으로 비밀번호 암호화 사용

--- src/main/java/com/sprint/mople/domain/user/service/UserService.java
+++ src/main/java/com/sprint/mople/domain/user/service/UserService.java
@@ -5,17 +5,20 @@ import com.sprint.mople.domain.user.dto.UserRegisterResponseDto;
 import com.sprint.mople.domain.user.entity.User;
 import com.sprint.mople.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
 public class UserService {
 
   private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
 
-  public UserService(UserRepository userRepository) {
+
+  public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
     this.userRepository = userRepository;
+    this.passwordEncoder = passwordEncoder;
   }
 
-  //회원가입 성공 Test
   public UserRegisterResponseDto registerUser(UserRegisterRequestDto request) {
     if (userRepository.existsByEmail(request.getEmail())) {
       throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
@@ -24,7 +27,9 @@ public class UserService {
     User user = new User();
     user.setUserName(request.getName());
     user.setEmail(request.getEmail());
-    user.setPassword(request.getPassword()); // 비밀번호 암호화 필요
+    //비밀번호 BCrypt 암호화
+    String encoded = passwordEncoder.encode(request.getPassword());
+    user.setPassword(encoded);
 
     User saved = userRepository.save(user);
 
