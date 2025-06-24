d1d598cb7d1442b9026953f83c008ddb0233990d
eunseobb <seobseob0810@naver.com>
2025-06-24 18:20:14 +0900

    refactor: FollowRepository 오타 수정

--- src/main/java/com/sprint/mople/domain/dm/repository/ChatRoomRepository.java
+++ src/main/java/com/sprint/mople/domain/dm/repository/ChatRoomRepository.java
@@ -1,10 +1,9 @@
 package com.sprint.mople.domain.dm.repository;
 
 import com.sprint.mople.domain.dm.entity.ChatRoom;
-import com.sprint.mople.domain.follow.entity.Follow;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChatRoomRpository extends JpaRepository<ChatRoom, UUID> {
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, UUID> {
 
 }
