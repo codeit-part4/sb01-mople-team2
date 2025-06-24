b7ebf231d477242f364864f53c729fe1705c0b74
janghoosa <janghoosa@gmail.com>
2025-06-24 09:13:20 +0900

    chore: move SecurityConfig

--- src/main/java/com/sprint/mople/global/config/SecurityConfig.java
+++ src/main/java/com/sprint/mople/global/config/SecurityConfig.java
@@ -1,11 +1,13 @@
-package com.sprint.mople.common.config;
+package com.sprint.mople.global.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
