13be1fa8a4040ccc895b5c56b12e410a5734400b
Haram0111 <5gkfka5@naver.com>
2025-06-25 14:46:31 +0900

    fix : google style 체크

--- src/main/java/com/sprint/mople/domain/user/controller/AuthController.java
+++ src/main/java/com/sprint/mople/domain/user/controller/AuthController.java
@@ -9,7 +9,10 @@ import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,7 +24,8 @@ public class AuthController {
 
 
   @PostMapping("/login")
-  public ResponseEntity<UserLoginResponseDto> login(@RequestBody @Valid UserLoginRequestDto request) {
+  public ResponseEntity<UserLoginResponseDto> login(
+      @RequestBody @Valid UserLoginRequestDto request) {
     UserLoginResponseDto response = userService.login(request.getEmail(), request.getPassword());
     return ResponseEntity.ok(response);
   }
