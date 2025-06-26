0e342be9645889c66c953f7b970e5968c954361d
Haram0111 <5gkfka5@naver.com>
2025-06-26 17:35:58 +0900

    refactoring: 에러 코드 수정 - ERROR 500에서 401로 수정

--- src/main/java/com/sprint/mople/domain/user/exception/LoginFailedException.java
+++ src/main/java/com/sprint/mople/domain/user/exception/LoginFailedException.java
@@ -1,5 +1,9 @@
 package com.sprint.mople.domain.user.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class LoginFailedException extends RuntimeException {
   public LoginFailedException(String message) {
     super(message);
