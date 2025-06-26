16f2f70eb58326540e6615160d541f5b7613f9d7
eunseobb <seobseob0810@naver.com>
2025-06-26 16:32:19 +0900

    refactor: fetch join 사용하여 n+1 방지, 참가자로 channel 조회하도록 변경

--- src/main/java/com/sprint/mople/domain/dm/repository/ChatRoomUserRepository.java
+++ src/main/java/com/sprint/mople/domain/dm/repository/ChatRoomUserRepository.java
@@ -1,5 +1,6 @@
 package com.sprint.mople.domain.dm.repository;
 
+import com.sprint.mople.domain.dm.entity.ChatRoom;
 import com.sprint.mople.domain.dm.entity.ChatRoomUser;
 import java.util.Optional;
 import java.util.UUID;
@@ -10,11 +11,12 @@ import org.springframework.data.repository.query.Param;
 public interface ChatRoomUserRepository extends JpaRepository<ChatRoomUser, UUID> {
 
   @Query("""
-          SELECT cu.chatRoom.id FROM ChatRoomUser cu
+          SELECT cu.chatRoom FROM ChatRoomUser cu
+          JOIN FETCH cu.chatRoom cr
           WHERE cu.user.id IN (:userId1, :userId2)
           GROUP BY cu.chatRoom.id
           HAVING COUNT(DISTINCT cu.user.id) = 2
       """)
-  Optional<UUID> findChatRoomIdByUserIds(@Param("userId1") UUID requestUserId,
+  Optional<ChatRoom> findChatRoomByUserIds(@Param("userId1") UUID requestUserId,
       @Param("userId2") UUID targetUserId);
 }
