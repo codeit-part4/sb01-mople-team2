d31325ed1320accf52180f30fb9b320ff327a82a
eunseobb <seobseob0810@naver.com>
2025-06-30 16:23:19 +0900

    docs: Swagger API 문서화 추가

--- src/main/java/com/sprint/mople/domain/dm/controller/MessageController.java
+++ src/main/java/com/sprint/mople/domain/dm/controller/MessageController.java
@@ -1,21 +1,17 @@
 package com.sprint.mople.domain.dm.controller;
 
 import com.sprint.mople.domain.dm.dto.MessageCreateRequest;
-import com.sprint.mople.domain.dm.dto.MessageResponse;
 import com.sprint.mople.domain.dm.service.MessageService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class MessageController {
-
-  private final SimpMessagingTemplate simpMessagingTemplate;
 
   private final MessageService messageService;
 
