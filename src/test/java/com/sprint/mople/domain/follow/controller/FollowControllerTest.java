5ad4584b1750108867ff5099bac71ae3db3e438e
eunseobb <seobseob0810@naver.com>
2025-07-07 16:17:34 +0900

    test: 팔로잉, 팔로워 페이지네이션 조회 성공 컨트롤러 테스트 추가

--- src/test/java/com/sprint/mople/domain/follow/controller/FollowControllerTest.java
+++ src/test/java/com/sprint/mople/domain/follow/controller/FollowControllerTest.java
@@ -1,31 +1,30 @@
 package com.sprint.mople.domain.follow.controller;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.StatusResultMatchersExtensionsKt.isEqualTo;
 
 import com.sprint.mople.domain.follow.dto.FollowResponse;
-import com.sprint.mople.domain.follow.repository.FollowRepository;
 import com.sprint.mople.domain.follow.service.FollowServiceImpl;
+import com.sprint.mople.domain.user.dto.UserListResponse;
 import com.sprint.mople.global.jwt.JwtProvider;
 import jakarta.servlet.http.HttpServletRequest;
+import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 @ExtendWith(MockitoExtension.class)
 class FollowControllerTest {
-
-  @Mock
-  FollowRepository followRepository;
 
   @Mock
   FollowServiceImpl followService;
@@ -41,6 +40,7 @@ class FollowControllerTest {
 
   @Test
   void 팔로우_성공() {
+
     // Given
     UUID followerId = UUID.randomUUID();
     UUID followeeId = UUID.randomUUID();
@@ -51,7 +51,8 @@ class FollowControllerTest {
         .thenReturn(followResponse);
 
     // When
-    ResponseEntity<FollowResponse> response = followController.follow(followeeId, httpServletRequest);
+    ResponseEntity<FollowResponse> response = followController.follow(followeeId,
+        httpServletRequest);
 
     // Then
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
@@ -61,6 +62,7 @@ class FollowControllerTest {
 
   @Test
   void 언팔로우_성공() {
+
     // Given
     UUID followerId = UUID.randomUUID();
     UUID followeeId = UUID.randomUUID();
@@ -74,11 +76,54 @@ class FollowControllerTest {
     verify(followService).unfollow(followerId, followeeId);
 
   }
+
   @Test
   void 팔로잉_전체_조회() {
+
+    // Given
+    UUID followerId = UUID.randomUUID();
+    UserListResponse userResponse = new UserListResponse("TestUser", "test@email.com", false,
+        Instant.now());
+    List<UserListResponse> userResponses = List.of(userResponse);
+    Page<UserListResponse> userResponsePage = new PageImpl<>(userResponses, PageRequest.of(0, 10),
+        userResponses.size());
+    when(httpServletRequest.getHeader("Authorization")).thenReturn("Bearer token");
+    when(jwtProvider.getUserId("token")).thenReturn(followerId);
+
+    when(followService.findAllFollowings(followerId)).thenReturn(userResponsePage);
+
+    // When
+    ResponseEntity<Page<UserListResponse>> response = followController.findAllFollowings(
+        httpServletRequest);
+
+    // Then
+    Page<UserListResponse> body = response.getBody();
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    assertThat(body.getContent().get(0).userName()).isEqualTo("TestUser");
   }
 
   @Test
   void 팔로워_전체_조회() {
+
+    // Given
+    UUID followeeId = UUID.randomUUID();
+    UserListResponse userResponse = new UserListResponse("TestUser", "test@email.com", false,
+        Instant.now());
+    List<UserListResponse> userResponses = List.of(userResponse);
+    Page<UserListResponse> userResponsePage = new PageImpl<>(userResponses, PageRequest.of(0, 10),
+        userResponses.size());
+    when(httpServletRequest.getHeader("Authorization")).thenReturn("Bearer token");
+    when(jwtProvider.getUserId("token")).thenReturn(followeeId);
+    when(followService.findAllFollowers(followeeId)).thenReturn(userResponsePage);
+
+    // When
+    ResponseEntity<Page<UserListResponse>> response = followController.findAllFollowers(
+        httpServletRequest);
+
+    // Then
+    Page<UserListResponse> body = response.getBody();
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    assertThat(body.getContent().get(0).userName()).isEqualTo("TestUser");
+
   }
 }
